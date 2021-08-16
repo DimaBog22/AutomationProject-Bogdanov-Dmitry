5948c8647e2416dd59055eabe0e68bc89f4ba6f0
Dzmitry <bogdanov111222@yandex.ru>
2021-08-16 17:04:21 +0300

    completed stream api filter task
    Дополнительное задание
    Создать коллекцию [1, 2, 3, 0, -1, 10, 11, -20, 40]
    Отфильтровать коллекцию по следующим критериям :
    	1. все элементы коллекции должны быть больше 0
    	2 .все элементы коллекции должны быть меньше 0
    	3. все элементы коллекции должны быть кратны 5
    	4. все элементы коллекции должны быть кратны 10
    Результат фильтрации вывести в консоль.

---
+++ src/test/java/Task_5_3.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Task_5_3 {
+
+    public static void main(String[] args) {
+
+        Integer [] intArr = {1, 2, 3, 0, -1, 10, 11, -20, 40};
+        List<Integer> intArrList = Arrays.asList(intArr);
+
+        System.out.println("original array list:");
+        System.out.println(intArrList);
+
+        List<Integer> integerListResult1 = intArrList.stream().filter(element -> element > 0).collect(Collectors.toList());
+        System.out.println("all elements are more than 0:");
+        System.out.println(integerListResult1);
+
+        List<Integer> integerListResult2 = intArrList.stream().filter(element -> element < 0).collect(Collectors.toList());
+        System.out.println("all elements are less than 0:");
+        System.out.println(integerListResult2);
+
+        List<Integer> integerListResult3 = intArrList.stream().filter(element -> element % 5 == 0 && element != 0).collect(Collectors.toList());
+        System.out.println("all elements are a multiple of five");
+        System.out.println(integerListResult3);
+
+        List<Integer> integerListResult4 = intArrList.stream().filter(element -> element % 10 == 0 && element != 0).collect(Collectors.toList());
+        System.out.println("all elements are a multiple of ten");
+        System.out.println(integerListResult4);
+
+
+
+    }
+
+}
