a91c07ae1387a6cc1949cb3f3908e054ced45320
Dzmitry <bogdanov111222@yandex.ru>
2021-08-24 21:03:40 +0300

    added gson library

--- src/test/java/WorkingWithJson.java
+++ src/test/java/WorkingWithJson.java
@@ -1,2 +1,20 @@
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class WorkingWithJson {
+
+    @Test
+    public void test1() {
+
+        Gson gson = new Gson();
+
+    }
+
+    public String readData() throws IOException {
+        return new String(Files.readAllBytes(Paths.get("src/test/java/data.json")));
+    }
+
 }
