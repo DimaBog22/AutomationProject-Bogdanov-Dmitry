c4486808896a4848838e4da110e203c4952595ef
Dzmitry <bogdanov111222@yandex.ru>
2021-08-26 10:36:39 +0300

    added explicit expectation for img and added new classes for td's in table

--- src/test/java/task8/Task_8.java
+++ src/test/java/task8/Task_8.java
@@ -4,16 +4,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
 
 public class Task_8 {
 
     WebDriver driver = null;
-    String link = "file:///D:/QA-%20automation%20%D0%BA%D1%83%D1%80%D1%81/%D0%B4%D0%B7%20%D0%BF%D0%BE%20%D0%B2%D0%B5%D1%80%D1%81%D1%82%D0%BA%D0%B5/contacts.html";
+    String link = "D:\\TMS automation project\\AutomationProject-Bogdanov-Dmitry\\src\\test\\java\\task8\\contacts.html";
 
     @BeforeTest
     public void preconditions(){
@@ -21,6 +24,7 @@ public class Task_8 {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(link);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
     }
 
@@ -30,7 +34,7 @@ public class Task_8 {
         // web elements
         WebElement vkLink = driver.findElement(By.linkText("VK")); // <a>
         WebElement addres = driver.findElement(By.cssSelector(".address")); // <p>
-        WebElement img = driver.findElement(By.cssSelector("[alt='Спецтехника из Японии']")); //<img>
+
         WebElement submitBtn = driver.findElement(By.cssSelector("input[type='submit']"));
 //        System.out.println(submitBtn.getAttribute("value")); // тк отправить - не текст, а значение атрибута value
         WebElement select = driver.findElement(By.tagName("select"));
@@ -39,7 +43,6 @@ public class Task_8 {
         WebElement inputName = driver.findElement(By.xpath("//*[@placeholder=\"Укажите ваше имя!\"]"));
         WebElement inputEmail = driver.findElement(By.xpath("//*[@type=\"email\"]"));
         WebElement textarea = driver.findElement(By.tagName("textarea"));
-
         // actions
         optionRegion.selectByIndex(1);
         deliveryсheckbox.click();
@@ -48,7 +51,11 @@ public class Task_8 {
         textarea.sendKeys("I want to by something");
         Thread.sleep(3000);
         submitBtn.click();
-
+        // explicit expectation
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[alt='Спецтехника из Японии']")));
+        WebElement img = driver.findElement(By.cssSelector("[alt='Спецтехника из Японии']"));
+        // verification
         Assert.assertTrue(img.isDisplayed());
 
     }
