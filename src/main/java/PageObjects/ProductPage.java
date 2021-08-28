f1c240d3e26db4f3d2cadc497a21d0ba0ac1c0b5
Dzmitry <bogdanov111222@yandex.ru>
2021-08-28 16:00:46 +0300

    added checkTitle method to the ProductPage and added standardLogin test

--- src/main/java/PageObjects/ProductPage.java
+++ src/main/java/PageObjects/ProductPage.java
@@ -1,6 +1,9 @@
 package PageObjects;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
+
+import java.util.Locale;
 
 public class ProductPage extends BasePage {
     private By title = By.cssSelector("[class=title]");
@@ -8,7 +11,18 @@ public class ProductPage extends BasePage {
     private By filter = By.tagName("select");
 
     public ProductPage verifyProductPage() {
+
         isDisplayed(title, logo, filter);
         return this;
+
     }
+
+    public ProductPage checkTitle() {
+
+        String actualResult = getElementText(title).toLowerCase(Locale.ROOT);
+        Assert.assertEquals(actualResult, "Products".toLowerCase());
+        return this;
+
+    }
+
 }
---
+++ src/test/java/Task_9.java
@@ -0,0 +1,30 @@
+import PageObjects.LoginPage;
+import PageObjects.ProductPage;
+import driver.BaseTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Task_9 extends BaseTest {
+
+    LoginPage loginPage = new LoginPage();
+    ProductPage productPage = new ProductPage();
+
+    @BeforeTest
+    public void preconditions(){
+        loginPage.openPage();
+    }
+
+    @Test
+    public void standardLogin(){
+
+        loginPage.
+                verifyLoginPage()
+                .loginToApplication("standard_user", "secret_sauce");
+
+        productPage.
+                verifyProductPage()
+                .checkTitle();
+
+    }
+
+}
