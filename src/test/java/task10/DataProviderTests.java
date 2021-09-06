ea9905cdffbe4d34d5016d7d3770955140b79c17
Dzmitry <bogdanov111222@yandex.ru>
2021-09-06 14:28:29 +0300

    added error text verification

--- src/test/java/task10/DataProviderTests.java
+++ src/test/java/task10/DataProviderTests.java
@@ -1,42 +1,53 @@
 package task10;
 
 import PageObjects.LoginPage;
+import PageObjects.ProductPage;
 import driver.BaseTest;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 
 public class DataProviderTests extends BaseTest {
 
     LoginPage loginPage = new LoginPage();
+    ProductPage productPage = new ProductPage();
     // data for tests
     String username = "standard_user";
     String password = "secret_sauce";
     String emptyField = "";
     String incorrectValue = "rrrddd23";
+    String emptyUsernameError = "Epic sadface: Username is required";
+    String emptyPasswordError = "Epic sadface: Password is required";
+    String incorrectValueError = "Epic sadface: Username and password do not match any user in this service";
+
 
     @DataProvider(name = "loginTests")
     public Object [][] loginInputData() {
 
         return new Object[][]{
 
-                {username, password}, // positive
-                {username, emptyField},
-                {emptyField, password},
-                {emptyField, emptyField},
-                {username, incorrectValue},
-                {incorrectValue, password},
-                {incorrectValue, incorrectValue}
+                {username, password, emptyField}, // positive
+                {username, emptyField, emptyPasswordError},
+                {emptyField, password, emptyUsernameError},
+                {emptyField, emptyField, emptyUsernameError},
+                {username, incorrectValue, incorrectValueError},
+                {incorrectValue, password, incorrectValueError},
+                {incorrectValue, incorrectValue, incorrectValueError}
 
         };
 
     }
     @Test(dataProvider = "loginTests")
-    public void loginVerification(String username, String password) {
+    public void loginVerification(String username, String password, @Optional("") String errorText) {
 
         loginPage.
                 openPage()
                 .verifyLoginPage()
                 .loginToApplication(username, password);
+        if (errorText.isEmpty()) {
+            productPage.verifyProductPage();
+        }
+
     }
 
 }
