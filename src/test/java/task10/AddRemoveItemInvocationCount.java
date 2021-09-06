966b25813387c240ca62ad1e3a7e92c5a84700de
Dzmitry <bogdanov111222@yandex.ru>
2021-09-06 14:52:04 +0300

    added AddRemoveItemInvocationCount class

---
+++ src/test/java/task10/AddRemoveItemInvocationCount.java
@@ -0,0 +1,17 @@
+package task10;
+
+import org.testng.annotations.Test;
+
+public class AddRemoveItemInvocationCount {
+
+    @Test
+    public void addItem() {
+
+    }
+
+    @Test
+    public void removeItem() {
+
+    }
+
+}
