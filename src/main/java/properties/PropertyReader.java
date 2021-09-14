e9c396076bef51dad08f10f249d3c76ce1933eba
Dzmitry <bogdanov111222@yandex.ru>
2021-09-14 14:34:47 +0300

    implemented logic of PropertyReader class and created config.properties file

--- src/main/java/properties/PropertyReader.java
+++ src/main/java/properties/PropertyReader.java
@@ -1,4 +1,28 @@
 package properties;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyReader {
+
+    private Properties properties;
+
+    public PropertyReader(Properties properties) {
+        this.properties = properties;
+    }
+
+    public Properties getProperties(String propertyName) {
+
+        Properties properties = new Properties();
+
+        try {
+            properties.load(getClass().getClassLoader().getResourceAsStream(propertyName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+
+    }
+
 }
