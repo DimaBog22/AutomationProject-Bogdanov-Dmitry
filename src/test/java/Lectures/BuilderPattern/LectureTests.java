eddbf0143477e6439ec30caf3d7f564cf0554703
Dzmitry <bogdanov111222@yandex.ru>
2021-09-16 21:54:38 +0300

    finished training with BuilderPattern

--- src/test/java/Lectures/BuilderPattern/LectureTests.java
+++ src/test/java/Lectures/BuilderPattern/LectureTests.java
@@ -1,4 +1,18 @@
 package Lectures.BuilderPattern;
 
 public class LectureTests {
+
+    public static void main(String[] args) {
+
+        PersonBuilder myPerson1 = new PersonBuilder.Builder()
+                .withName("Dmitry")
+                .withLastName("Bogdanov")
+                .withSex("male")
+                .withAge(27)
+                .build();
+
+        System.out.println(myPerson1.toString());
+        
+    }
+
 }
--- src/test/java/Lectures/BuilderPattern/PersonBuilder.java
+++ src/test/java/Lectures/BuilderPattern/PersonBuilder.java
@@ -7,12 +7,50 @@ public class PersonBuilder {
     Integer age;
     String sex;
 
+    @Override
+    public String toString() {
+        return "PersonBuilder{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+
     public static class Builder {
 
         private PersonBuilder personBuilder;
 
         public Builder() {
             personBuilder = new PersonBuilder();
+        }
+
+        public Builder withName(String name) {
+
+            personBuilder.name = name;
+            return this;
+
+        }
+
+        public Builder withLastName(String lastname) {
+
+            personBuilder.lastname = lastname;
+            return this;
+
+        }
+
+        public Builder withSex(String sex) {
+
+            personBuilder.sex = sex;
+            return this;
+
+        }
+
+        public Builder withAge(Integer age) {
+
+            personBuilder.age = age;
+            return this;
+
         }
 
         public PersonBuilder build() {
