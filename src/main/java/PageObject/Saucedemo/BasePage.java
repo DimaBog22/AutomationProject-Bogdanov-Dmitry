314d066d6257e065308a697965b743a48081e10f
Dzmitry <bogdanov111222@yandex.ru>
2021-09-22 15:25:38 +0300

    changed pom and basepage

--- src/main/java/PageObject/Saucedemo/BasePage.java
+++ src/main/java/PageObject/Saucedemo/BasePage.java
@@ -1,10 +1,12 @@
 package PageObject.Saucedemo;
 
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import properties.PropertyReader;
 import java.util.Locale;
 import static driver.DriverCreation.getDriver;
+@Log4j2
 
 public class BasePage {
 
@@ -17,10 +19,14 @@ public class BasePage {
     protected void enter(By element, CharSequence... charSequences) {
         getDriver().findElement(element).clear();
         getDriver().findElement(element).sendKeys(charSequences);
+        log.info(element + " has value " + charSequences);
     }
 
     protected void click(By element) {
+
         getDriver().findElement(element).click();
+        log.info(element + " was clicked");
+
     }
 
     protected void openUsingProperties(String url) {
@@ -28,7 +34,10 @@ public class BasePage {
     }
 
     protected String getElementText(By element) {
+
+        log.info(element + " has value inside " + getDriver().findElement(element).getText());
         return getDriver().findElement(element).getText();
+
     }
 
     protected Integer getCountElements(By element) {
@@ -44,6 +53,7 @@ public class BasePage {
     protected Boolean isElementPresented(By element){
 
         if(getDriver().findElements(element).size() != 0) {
+            log.info(element + " has size " + getDriver().findElements(element).size());
             return true;
         }
         return false;
@@ -53,15 +63,18 @@ public class BasePage {
     protected void checkTitle(By element, String expectedResult) {
 
         String actualResult = getElementText(element).toLowerCase(Locale.ROOT);
+        log.info("title is " + actualResult);
         Assert.assertEquals(actualResult, expectedResult.toLowerCase(Locale.ROOT));
 
     }
     // using config.properties file
     protected void openUsingProperties() {
+        log.info("link is opened with properties file");
         getDriver().get(appProperties.getProperties().getProperty("url"));
     }
 
     protected void open(String url) {
+        log.info(url + "is opened");
         getDriver().get(url);
     }
 
