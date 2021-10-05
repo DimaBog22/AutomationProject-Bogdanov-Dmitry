8b37280cabbbec51e44250588a88090f34f00055
Dzmitry <bogdanov111222@yandex.ru>
2021-10-05 19:52:54 +0300

    Create AppPropertiesLoginTest.java

---
+++ src/test/java/ApplicationPropertiesTests/AppPropertiesLoginTest.java
@@ -0,0 +1,33 @@
+package ApplicationPropertiesTests;
+
+import PageObject.Saucedemo.LoginPage;
+import PageObject.Saucedemo.ProductPage;
+import driver.BaseTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import properties.PropertyReader;
+
+public class AppPropertiesLoginTest extends BaseTest {
+    LoginPage loginPage = new LoginPage();
+    ProductPage productPage = new ProductPage();
+
+    // using config.properties file
+    PropertyReader appProperties = new PropertyReader("config/config.properties");
+    String username = appProperties.getProperties().getProperty("login");
+    String password = appProperties.getProperties().getProperty("password");
+
+    @BeforeMethod
+    public void precondition() {
+        loginPage.openPage();
+    }
+
+    @Test()
+    public void loginToApplicationStandard_Test() throws InterruptedException {
+        loginPage.openPage()
+                .verifyLoginPage()
+                .loginToApplication(username, password);
+        productPage.verifyProductPage();
+    }
+
+}
+
